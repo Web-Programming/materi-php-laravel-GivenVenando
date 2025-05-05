bd850ef8b2016c68a17bb0492f0d3475c43f1a5f
JkOmxr <143261728+JkOmxr@users.noreply.github.com>
2025-05-05 14:54:58 +0700

    Controller/Layout

---
+++ latihan/resources/views/materi/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.master')
+
+@section('contend')
+    <p>Ini Adalah Halaman Index Materi</p>
+    <p>Isinya list materi dari database</p>
+@endsection
+
+@endsection('title','Ini Halaman List Materi')
--- latihan/routes/web.php
+++ latihan/routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MateriController;
+use App\Http\Controllers\ProdiController;
+use App\Http\Controllers\MhsApiController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('welcome');
+});
+
+Route::get('/materi/index',action:[MateriController::class,'index']);
+
+Route::get('/materi/index/{id}',action:[MateriController::class,'detail']);
+
+Route::resource('prodi',ProdiController::class,);
+
+Route::apiResource('api/mhs',MhsController::class);
+
+
